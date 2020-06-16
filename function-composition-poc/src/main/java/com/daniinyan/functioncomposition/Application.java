65ed7361ff134eebf04d565de444f8ab1e5ec5e6
DaniiNyan <daniela.araujo@ilegra.com>
2020-06-16 00:00:42 -0300

    first commit with simple functions

---
+++ function-composition-poc/src/main/java/com/daniinyan/functioncomposition/Application.java
@@ -0,0 +1,17 @@
+package com.daniinyan.functioncomposition;
+
+import java.util.function.Function;
+
+public class Application {
+
+  public static void main(String[] args) {
+
+    Integer baseNumber = 4;
+    Function<Integer, Integer> multiplyByTwo = number -> number * 2;
+    System.out.println(baseNumber + " * 2 = " + multiplyByTwo.apply(baseNumber));
+
+    Function<Integer, Integer> divideByTwo = number -> number / 2;
+    System.out.println(baseNumber + " / 2 = " + divideByTwo.apply(baseNumber));
+  }
+
+}
