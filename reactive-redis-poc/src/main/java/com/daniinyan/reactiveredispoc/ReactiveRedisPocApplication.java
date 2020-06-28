6b346d6a7dd4656dcb67bac4ef9ff3cd94415e7f
DaniiNyan <daniela.araujo@ilegra.com>
2020-06-28 19:26:59 -0300

    first commit

---
+++ reactive-redis-poc/src/main/java/com/daniinyan/reactiveredispoc/ReactiveRedisPocApplication.java
@@ -0,0 +1,13 @@
+package com.daniinyan.reactiveredispoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveRedisPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveRedisPocApplication.class, args);
+	}
+
+}
