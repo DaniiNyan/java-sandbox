b11fe448fdac1213c8e5584d69e9137ff543425f
DaniiNyan <daniela.araujo@ilegra.com>
2020-06-29 22:47:06 -0300

    add another use to functional interface

--- function-composition-poc/src/main/java/com/daniinyan/functioncomposition/FunctionalInterfaces.java
+++ function-composition-poc/src/main/java/com/daniinyan/functioncomposition/FunctionalInterfaces.java
@@ -15,8 +15,11 @@ public class FunctionalInterfaces {
   public static void main(String[] args) {
     Book myBook = new Book("My Book", "Me", 2020);
 
-    BookDisplay bookDisplay = book -> System.out.println(book.getName());
-    bookDisplay.print(myBook);
+    BookDisplay bookName = book -> System.out.println(book.getName());
+    bookName.print(myBook);
+
+    BookDisplay bookAuthor = book -> System.out.println(book.getAuthor());
+    bookAuthor.print(myBook);
 
     Square square = (int number) -> number * number;
     System.out.println("Result: " + square.calculate(4));
